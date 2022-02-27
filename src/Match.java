78f5f2e06c442d74adc42437c14fc1e621388fc3
Gülsün Görgülü <gorgulugulsun@gmail.com>
2022-02-27 22:09:18 +0300

    Initial commit

---
+++ src/Match.java
@@ -0,0 +1,18 @@
+public class Match {
+
+    String  homeTeam;
+    String  awayTeam;
+
+    public Match(String homeTeam, String awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+}
